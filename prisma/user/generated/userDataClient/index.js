2f2a9a15d91bc2c3c1124d0bf9476cc1f78108fa
JaehunCheon <dhsslwogns@naver.com>
2024-06-04 16:39:42 +0900

    test commit

--- prisma/user/generated/userDataClient/index.js
+++ prisma/user/generated/userDataClient/index.js
@@ -152,7 +152,7 @@ const config = {
     "isCustomOutput": true
   },
   "relativeEnvPaths": {
-    "rootEnvPath": null,
+    "rootEnvPath": "../../../../.env",
     "schemaEnvPath": "../../../../.env"
   },
   "relativePath": "../..",
