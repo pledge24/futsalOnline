aaf0c76391c7ed0604a08fc40807548c9676ae3a
lucetaseo <101718150+lucetaseo@users.noreply.github.com>
2024-06-05 19:59:35 +0900

    수우정
    수정

--- src/routes/cash.router.js
+++ src/routes/cash.router.js
@@ -11,26 +11,15 @@ router.patch("/shop/charge/:Character_Id", authMiddleware, async (req, res) => {
   const Character_Id = +req.params.Character_Id;
   const { amount } = req.body; // 충전할 금액을 요청 본문에서 가져옵니다.
 
-  if (!amount || typeof amount !== 'number' || amount <= 0) {
-    return res.status(400).json({ errorMessage: "충전 금액은 0보다 큰 숫자여야 합니다." });
+  if (!amount || typeof amount !== 'number' || amount <= 1000) {
+    return res.status(400).json({ errorMessage: "충전 금액은 1,000원 보다 큰 숫자여야 합니다." });
   }
 
   try {
-    // 캐릭터 정보 가져오기
-    const character = await Characters.findUnique({
-      where: {
-        Character_Id,
-      },
-    });
-
-    if (!character) {
-      return res.status(404).json({ errorMessage: "캐릭터를 찾을 수 없습니다." });
-    }
-
     // 캐쉬 충전
     const newBalance = character.cash + amount;
 
-    await Characters.update({
+    await userClient.userinfo.update({
       data: {
         cash: newBalance,
       },
@@ -39,15 +28,6 @@ router.patch("/shop/charge/:Character_Id", authMiddleware, async (req, res) => {
       },
     });
 
-    // 충전 기록 저장
-    await CashTransactions.create({
-      data: {
-        Character_Id: Character_Id,
-        amount: amount,
-        transactionType: 'charge', // 충전 유형
-        createdAt: new Date(),
-      },
-    });
 
     return res.status(200).json({
       message: `캐쉬가 성공적으로 충전되었습니다.`,
